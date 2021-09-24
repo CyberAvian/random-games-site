0c16d9db4486379927c73794d235fc79b7a1c15d
cyberavian <cyberavian025@gmail.com>
2021-09-24 10:01:43 -0400

    updated css

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,7 +1,13 @@
 let gameOver = false;
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0;
 let cells = new Array(3).fill("").map(() => new Array(3).fill(""));
 let gameBoard = document.querySelector("#game-board");
 let gameBoardText = gameBoard.querySelector("p");
+let playerWinCount = document.querySelector("#player-win-count");
+let drawCount = document.querySelector("#draw-count");
+let computerWinCount = document.querySelector('#computer-win-count');
 createGameBoard();
 
 let newGame = document.querySelector("#new-game");
@@ -19,10 +25,10 @@ function createGameBoard() {
             columnElement.classList = "cell";
     
             if (rowNum !== 2) {
-                columnElement.style.borderBottom = "solid 1px black";
+                columnElement.style.borderBottom = "solid 1px white";
             }
             if (columnNum !== 2) {
-                columnElement.style.borderRight = "solid 1px black";
+                columnElement.style.borderRight = "solid 1px white";
             } 
 
             columnElement.addEventListener('click', playRound);
@@ -36,16 +42,24 @@ function createGameBoard() {
     
         gameBoard.appendChild(rowElement);
     }
+
+    playerWinCount.textContent = `Player: ${playerWins}`;
+    drawCount.textContent = `Draw: ${draws}`;
+    computerWinCount.textContent = `Computer: ${computerWins}`;
 }
 
 function clearGameBoard() {
     let cells = document.querySelectorAll(".cell");
     cells.forEach(cell => {
+        cell.classList.remove("player-win");
+        cell.classList.remove("computer-win");
         let cellTextElement = cell.querySelector("p");
         cellTextElement.textContent = "";
     });
     gameOver = false;
     gameBoardText.textContent = "";
+    gameBoardText.style.background = "none";
+    gameBoardText.style.backgroundClip = "none";
 }
 
 function playRound() {
@@ -54,13 +68,19 @@ function playRound() {
     let cellTextElement = this.querySelector("p");
     if (cellTextElement.textContent !== "") return;
     cellTextElement.textContent = "X";
-    cellTextElement.style.color = "blue";
+    cellTextElement.style.color = "#549BCD";
     
     if (checkWin()) {
+        gameBoardText.style.color = "#549BCD";
         gameBoardText.textContent = "You win!";
+        playerWins++;
         gameOver = true;
     } else if (countBlankCells() === 0) {
+        gameBoardText.style.background = "linear-gradient(#CF6060, #549BCD)";
+        gameBoardText.style.backgroundClip = "text";
+        gameBoardText.style.color = "transparent";
         gameBoardText.textContent = "Draw";
+        draws++;
         gameOver = true;
     } else {
         let computerPlayed;
@@ -68,10 +88,16 @@ function playRound() {
             computerPlayed = computerMove();
         } while (!computerPlayed)
         if (checkWin()) {
+            gameBoardText.style.color = "#CF6060";
             gameBoardText.textContent = "You lose!";
+            computerWins++;
             gameOver = true;
         }
     }
+
+    playerWinCount.textContent = `Player: ${playerWins}`;
+    drawCount.textContent = `Draw: ${draws}`;
+    computerWinCount.textContent = `Computer: ${computerWins}`;
 }
 
 function computerMove() {
@@ -82,7 +108,7 @@ function computerMove() {
     let cellText = computerCell.querySelector("p");
     if (cellText.textContent === "") {
         cellText.textContent = "O";
-        cellText.style.color = "red";
+        cellText.style.color = "#CF6060";
         return true;
     }
     return false;
@@ -124,7 +150,15 @@ function checkWin() {
             }
         }
 
-        if (xCount === 3 || oCount === 3) {
+        if (xCount === 3) {
+            cells[row].forEach(col => {
+                col.classList.add("player-win");
+            });
+            return true;
+        } else if (oCount === 3) {
+            cells[row].forEach(col => {
+                col.classList.add("computer-win");
+            });
             return true;
         }
     }
@@ -147,8 +181,15 @@ function checkWin() {
             }
         }
 
-        console.log(`Col xCount ${xCount} oCount ${oCount}`)
-        if (xCount === 3 || oCount === 3) {
+        if (xCount === 3) {
+            for (let row = 0; row < 3; row++) {
+                cells[row][col].classList.add("player-win");
+            }
+            return true;
+        } else if (oCount === 3) {
+            for (let row = 0; row < 3; row++) {
+                cells[row][col].classList.add("computer-win");
+            }
             return true;
         }
     }
@@ -156,14 +197,33 @@ function checkWin() {
     // Check Diagonal
     if (cells[0][0].textContent !== "" && cells[1][1].textContent !== "" && cells[2][2].textContent !== "") {
         if (cells[0][0].textContent === cells[1][1].textContent && cells[0][0].textContent === cells[2][2].textContent) {
+            if (cells[0][0].textContent === 'X') {
+                cells[0][0].classList.add("player-win");
+                cells[1][1].classList.add("player-win");
+                cells[2][2].classList.add("player-win");
+            } else if (cells[0][0].textContent === 'O') {
+                cells[0][0].classList.add("computer-win");
+                cells[1][1].classList.add("computer-win");
+                cells[2][2].classList.add("computer-win");
+            }
             return true;
         }
     } else if (cells[0][2].textContent !== "" && cells[2][0].textContent !== "") {
         if (cells[0][2].textContent === cells[1][1].textContent && cells[0][2].textContent === cells[2][0].textContent) {
+            if (cells[0][2].textContent === 'X') {
+                cells[0][2].classList.add("player-win");
+                cells[1][1].classList.add("player-win");
+                cells[2][0].classList.add("player-win");
+            } else if (cells[0][2].textContent === 'O') {
+                cells[0][2].classList.add("computer-win");
+                cells[1][1].classList.add("computer-win");
+                cells[2][0].classList.add("computer-win");
+            }
+ 
             return true;
         }
     }
 
-    // No winning moves on board
+    // No` winning moves on board
     return false;
 }
